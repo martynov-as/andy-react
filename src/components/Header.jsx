728b792609592558617d3ad931e603e9ad13d610
martynov-as <60813284+martynov-as@users.noreply.github.com>
2021-05-23 19:59:20 +0300

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+	return (
+		<header className='header'>
+			<img src='https://upload.wikimedia.org/wikipedia/commons/thumb/7/79/Vanamo_Logo.svg/1200px-Vanamo_Logo.svg.png' />
+		</header>
+	)
+}
+
+export default Header;
