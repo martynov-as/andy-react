fe55dcec88106fc4f4bbb4fc70a69be020a4dda5
Andy <martynov.as@yandex.ru>
2021-05-27 19:46:35 +0300

    27.05

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,10 +1,41 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import styles from "./Dialogs.module.css";
 
 const Dialogs = () => {
 	return (
-		<div>
-			Dialogs
+		<div className={styles.dialogs}>
+			<div className={styles.dialogsItems}>
+				<div className={styles.dialog + " " + styles.active}>
+					<NavLink to="/dialogs/1">Dimych</NavLink>
+				</div>
+				<div className={styles.dialog}>
+					<NavLink to="/dialogs/2">Andrey</NavLink>
+				</div>
+				<div className={styles.dialog}>
+					<NavLink to="/dialogs/3">Sveta</NavLink>
+				</div>
+				<div className={styles.dialog}>
+					<NavLink to="/dialogs/4">Sacha</NavLink>
+				</div>
+				<div className={styles.dialog}>
+					<NavLink to="/dialogs/5">Viktor</NavLink>
+				</div>
+				<div className={styles.dialog}>
+					<NavLink to="/dialogs/6">Valera</NavLink>
+				</div>
+			</div>
+			<div className={styles.messages}>
+				<div className={styles.message}>
+					Hi
+				</div>
+				<div className={styles.message}>
+					How is your
+				</div>
+				<div className={styles.message}>
+					Yo
+				</div>
+			</div>
 		</div>
 	)
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,26 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import styles from "./Navbar.module.css";
 
 const Navbar = () => {
 	return (
 		<nav className={styles.navbar}>
 			<ul>
-				<li className={styles.item}><a href='/profile'>Profile</a></li>
-				<li className={styles.item}><a href='/dialogs'>Messages</a></li>
-				<li className={styles.item}><a href='/news'>News</a></li>
-				<li className={styles.item}><a href='/music'>Music</a></li>
-				<li className={styles.item}><a href='/settings'>Settings</a></li>
+				<li className={styles.item}>
+					<NavLink to='/profile' activeClassName={styles.activeLink}>Profile</ NavLink>
+				</li>
+				<li className={styles.item}>
+					<NavLink to='/dialogs' activeClassName={styles.activeLink}>Messages</ NavLink>
+				</li>
+				<li className={styles.item}>
+					<NavLink to='/news' activeClassName={styles.activeLink}>News</ NavLink>
+				</li>
+				<li className={styles.item}>
+					<NavLink to='/music' activeClassName={styles.activeLink}>Music</ NavLink>
+				</li>
+				<li className={styles.item}>
+					<NavLink to='/settings' activeClassName={styles.activeLink}>Settings</ NavLink>
+				</li>
 			</ul>
 		</nav>
 	)
